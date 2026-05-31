c161a1932225e5069f3459fb09d88a03b1107516
Will Bennion <wibble199@users.noreply.github.com>
2026-05-31 12:29:56 +0000

    Automatic elevation change now allows tokens to fit under elevated terrain

--- src/automation/token-elevation.mjs
+++ src/automation/token-elevation.mjs
@@ -1,28 +1,30 @@
+/** @import { TerrainShape } from "../geometry/terrain-shape.mjs" */
 import { tokenElevationChange$ } from "../config/settings.mjs";
 import { moduleName, tokenFlags } from "../consts.mjs";
 import { heightMap } from "../geometry/height-map.mjs";
 import { getTerrainType } from "../stores/terrain-types.mjs";
 import { getSpacesUnderToken, toSceneUnits } from "../utils/grid-utils.mjs";
+import { getTokenHeight } from "../utils/token-utils.mjs";
 
 // TODO: make this configurable?
 const automaticElevationMovementTypes = ["walk", "crawl", "climb", "jump", "teleport", "blink"];
 
-const thtElevation = Symbol("thtElevation");
+const thtTerrainTop = Symbol("thtTerrainTop");
 
 /**
  * Wrapper that handles automatic elevation change when the user is dragging the token within a scene.
  * This is not used in other circumstances such as when using the keyboard or undoing a previous move.
  * @param {(current, changes, options) => { elevation: number; x: number; y: number; }} wrapped
- * @param {{ x: number; y: number; elevation: number; action: string | undefined; [thtElevation]?: number; }} current
+ * @param {{ x: number; y: number; elevation: number; action: string | undefined; [thtTerrainTop]?: number; }} current
  * @param {{ x: number; y: number; }} changes
  * @param {any} options
  */
 export function tokenGetDragWaypointPositionWrapper(wrapped, current, changes, options) {
 	const position = wrapped(current, changes, options);
 
-	const autoElevationEnabled = tokenElevationChange$.value &&
-		!this.document._source.flags?.[moduleName]?.[tokenFlags.ignoreAutoElevation] &&
-		automaticElevationMovementTypes.includes(this.document.movementAction);
+	const autoElevationEnabled = tokenElevationChange$.value
+		&& !this.document._source.flags?.[moduleName]?.[tokenFlags.ignoreAutoElevation]
+		&& automaticElevationMovementTypes.includes(this.document.movementAction);
 	if (!autoElevationEnabled) return position;
 
 	// `current` is the same object that was returned from this function the last time it was called.
@@ -30,13 +32,27 @@ export function tokenGetDragWaypointPositionWrapper(wrapped, current, changes, o
 	// So, record the THT elevation from the previous run, then we can calculate the delta against just the THT
 	// elevation, and add that to the position's elevation without affecting elevation changes from +/- keys (or other).
 	const { x: initialX, y: initalY, width, height, shape } = this.document._source;
+	const tokenZHeight = getTokenHeight(this.document);
 
-	const previousElevation = current[thtElevation] ?? getHighestTerrainUnderToken({ x: initialX, y: initalY, width, height, shape });
-	const currentElevation = getHighestTerrainUnderToken({ ...position, width, height, shape });
+	// Top of the terrain under the token in the `current` position.
+	// We ignore any terrain that is is entirely above the token, i.e. if the token is in a gap beneath this terrain
+	const currentTerrainTop = current[thtTerrainTop]
+		?? getTopMostTerrainUnderToken(
+			{ x: initialX, y: initalY, width, height, shape },
+			{ terrainFilter: s => s.bottom <= current.elevation + tokenZHeight }
+		);
 
-	const delta = currentElevation - previousElevation;
+	const currentElevationAboveTerrain = current.elevation - currentTerrainTop;
+
+	// Find the best suitable space for the token, attempting to the keep the same relative height above terrain
+	const nextTerrainTop = getTopMostTerrainUnderToken(
+		{ ...position, width, height, shape },
+		{ gapSearch: { currentTerrainTop, currentElevationAboveTerrain, tokenZHeight } }
+	);
+
+	const delta = nextTerrainTop - currentTerrainTop;
 	position.elevation += toSceneUnits(delta);
-	position[thtElevation] = currentElevation;
+	position[thtTerrainTop] = nextTerrainTop;
 
 	return position;
 }
@@ -51,14 +67,16 @@ export function tokenGetDragWaypointPositionWrapper(wrapped, current, changes, o
 export function tokenGetShiftedPositionWrapper(wrapped, dx, dy, dz) {
 	const position = wrapped(dx, dy, dz);
 
-	const autoElevationEnabled = tokenElevationChange$.value &&
-		!this.document._source.flags?.[moduleName]?.[tokenFlags.ignoreAutoElevation] &&
-		automaticElevationMovementTypes.includes(this.document.movementAction);
+	const autoElevationEnabled = tokenElevationChange$.value
+		&& !this.document._source.flags?.[moduleName]?.[tokenFlags.ignoreAutoElevation]
+		&& automaticElevationMovementTypes.includes(this.document.movementAction);
 	if (!autoElevationEnabled) return position;
 
-	const { width, height, shape } = this.document._source;
-	const initialElevation = getHighestTerrainUnderToken(this.document._source);
-	const newElevation = getHighestTerrainUnderToken({ x: position.x, y: position.y, width, height, shape });
+	const { width, height, shape, elevation } = this.document._source;
+	const tokenZHeight = getTokenHeight(this.document);
+
+	const initialElevation = getTopMostTerrainUnderToken(this.document._source, { elevation, height: tokenZHeight });
+	const newElevation = getTopMostTerrainUnderToken({ x: position.x, y: position.y, width, height, shape }, { elevation, height: tokenZHeight });
 
 	position.elevation += toSceneUnits(newElevation - initialElevation);
 
@@ -78,18 +96,34 @@ export function onTokenPreCreate(tokenDoc, _createData, _options, userId) {
 	// If the token has the ignore auto-elevation flag set, skip initial elevation
 	if (tokenDoc.getFlag(moduleName, tokenFlags.ignoreAutoElevation)) return;
 
-	const terrainHeight = getHighestTerrainUnderToken(tokenDoc);
+	const terrainHeight = getTopMostTerrainUnderToken(tokenDoc);
 
 	tokenDoc.updateSource({ elevation: terrainHeight });
 }
 
 /**
  * Finds the highest solid terrain point under the given token position. This accounts for terrain height and elevation.
+ *
+ * On gridded scenes, when using the arrow keys to move, Foundry will round a token's elevation to the nearest whole
+ * grid scale distance. E.G. on a scene where distaince is 5ft, setting a token to have an elevation of 2.5 and then
+ * moving it to another space using the keyboard causes the elevation to jump to 5. This does not seem to happen if
+ * snapping is disabled or on gridless scenes. This behavior causes problems when using fractional height terrain, since
+ * THT would adjust the elevation to be fraction, then if the user moves the token it then rounds that off. E.G. moving
+ * a token on and off a half height terrain using the keyboard would cause the token to keep rising and rising. To get
+ * around this, we round off all the elevation changes that THT does. This isn't ideal as it means fractional height
+ * terrains don't behave as expected, but it does resolve the issue of indefinitely rising tokens.
  * @param {{ x: number; y: number; width: number; height: number; shape: number; }} position
+ * @param {Object} [options]
+ * @param {(terrain: TerrainShape) => boolean} [options.terrainFilter] If provided, only terrain passing this predicate
+ * is considered.
+ * @param {{ currentTerrainTop: number; currentElevationAboveTerrain: number; tokenZHeight: number }} [options.gapSearch]
  */
-function getHighestTerrainUnderToken(position) {
+function getTopMostTerrainUnderToken(position, { terrainFilter, gapSearch } = {}) {
 	const { x, y, width, height, shape } = position;
 	const { type: gridType, size: gridSize } = canvas.grid;
+
+	/** @type {{ bottom: number; top: number; }[]} */
+	const occuipedTerrainRanges = [{ bottom: -Infinity, top: 0 }];
 
 	let highest = 0;
 
@@ -100,18 +134,42 @@ function getHighestTerrainUnderToken(position) {
 		for (const shape of shapes) {
 			const terrainType = getTerrainType(shape.terrainTypeId);
 			if (!terrainType?.usesHeight || !terrainType.isSolid) continue; // zone or non solid, ignore it
+			if (typeof terrainFilter === "function" && !terrainFilter(shape)) continue;
 
 			highest = Math.max(highest, shape.top);
+
+			occuipedTerrainRanges.push({
+				bottom: gridType === CONST.GRID_TYPES.GRIDLESS ? shape.bottom : Math.round(shape.bottom),
+				top: gridType === CONST.GRID_TYPES.GRIDLESS ? shape.top : Math.round(shape.top)
+			});
 		}
 	}
 
-	// On gridded scenes, when using the arrow keys to move, Foundry will round a token's elevation to the nearest whole
-	// grid scale distance. E.G. on a scene where distaince is 5ft, setting a token to have an elevation of 2.5 and then
-	// moving it to another space using the keyboard causes the elevation to jump to 5. This does not seem to happen if
-	// snapping is disabled or on gridless scenes. This behavior causes problems when using fractional height terrain,
-	// since THT would adjust the elevation to be fraction, then if the user moves the token it then rounds that off.
-	// E.G. moving a token on and off a half height terrain using the keyboard would cause the token to keep rising and
-	// rising. To get around this, we round off all the elevation changes that THT does. This isn't ideal as it means
-	// fractional height terrains don't behave as expected, but it does resolve the issue of indefinitely rising tokens.
-	return gridType === CONST.GRID_TYPES.GRIDLESS ? highest : Math.round(highest);
+	// If we're only looking for the highest possible terrain, return that
+	if (!gapSearch)
+		return gridType === CONST.GRID_TYPES.GRIDLESS ? highest : Math.round(highest);
+
+	// Otherwise if we're looking for the lowest gap, sort the terrain and loop through to find a gap big enough for the
+	// token.
+	if (!occuipedTerrainRanges.length) return 0;
+
+	occuipedTerrainRanges.sort((a, b) => a.bottom - b.bottom || a.top - b.top);
+
+	// Required gap size if we wish to maintain the same relative elevation above terrain.
+	// TODO: add configuration that allows token to "squeeze"?
+	// TODO: add configuration that will ignore elevation above terrain for these purposes?
+	const requiredGapSize = gapSearch.currentElevationAboveTerrain + gapSearch.tokenZHeight;
+
+	for (let i = 0; i < occuipedTerrainRanges.length - 1; i++) {
+		const gapSize = occuipedTerrainRanges[i + 1].bottom - occuipedTerrainRanges[i].top;
+		if (gapSize < requiredGapSize) continue;
+
+		// For the gap to be valid, the top-most point of the gap must not be lower than the current elevation plus
+		// the token's vertical height - otherwise the token could not feasibly move through that space into the gap.
+		if (occuipedTerrainRanges[i + 1].bottom < gapSearch.currentTerrainTop + gapSearch.tokenZHeight) continue;
+
+		return occuipedTerrainRanges[i].top;
+	}
+
+	return occuipedTerrainRanges.at(-1).top;
 }
