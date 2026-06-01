Agraael/FoundryVTT-Terrain-Height-Tools

commit 120b7253c5d1f4bfd4247ff38110e36854d66ab4

Fix zones not merging correctly
Closes #40.

src/automation/scene-regions.mjs
@@ -65,13 +65,9 @@ async function createOrUpdateRegions(terrainShapes, cleanup = false) {
 	// If cleaning up, do this first as this will remove duplicate scene regions that have been erroneously created with the same type/top/bottom
 	// Hopefully this should never happen, but it's here as a failsafe.
 	if (cleanup) {
-		const neededRegions = new Set([...allTerrainShapes$.value].map(s => getMapKey(s.terrainTypeId, s.top, s.bottom)));
+		const neededRegions = new Set([...allTerrainShapes$.value].map(getMapKeyForShape));
 		for (const region of getThtSceneRegions()) {
-			const regionKey = getMapKey(
-				region.flags[moduleName]?.[regionFlags.terrainTypeId],
-				region._source.elevation.top,
-				region._source.elevation.bottom
-			);
+			const regionKey = getMapKeyForRegion(region);
 
 			// Delete the regionKey from the neededRegions set so that if we encounter another region with the same type/top/bottom, we delete it
 			if (!neededRegions.delete(regionKey)) {
@@ -89,19 +85,9 @@ async function createOrUpdateRegions(terrainShapes, cleanup = false) {
 		.map(({ terrainTypeId, top, bottom }) => ({ terrainTypeId, top, bottom }));
 
 	/** @type {Map<string, RegionDocument>} */
-	const existingRegionsLookup = new Map(getThtSceneRegions().map(r => [
-		getMapKey(
-			r.flags[moduleName]?.[regionFlags.terrainTypeId],
-			r._source.elevation.top, // use ._source otherwise this is Infinity instead of null for boundless regions
-			r._source.elevation.bottom
-		),
-		r
-	]));
+	const existingRegionsLookup = new Map(getThtSceneRegions().map(r => [getMapKeyForRegion(r), r]));
 
-	const terrainShapesLookup = groupBy(allTerrainShapes$.value, s => {
-		const usesHeight = terrainTypeMap$.value.get(s.terrainTypeId)?.usesHeight;
-		return getMapKey(s.terrainTypeId, usesHeight ? s.top : null, usesHeight ? s.bottom : null);
-	});
+	const terrainShapesLookup = groupBy(allTerrainShapes$.value, getMapKeyForShape);
 
 	for (const { terrainTypeId, top, bottom } of changedRegions) {
 		const mapKey = getMapKey(terrainTypeId, top, bottom);
@@ -206,11 +192,27 @@ function getThtSceneRegions() {
 	return canvas.scene.regions.filter(r => r.flags[moduleName]?.[regionFlags.terrainTypeId]);
 }
 
-/**
- * @param {string} terrainTypeId
- * @param {number} top
- * @param {number} bottom
- */
+/** @param {Region} region */
+function getMapKeyForRegion(region) {
+	// use ._source otherwise this is Infinity instead of null for boundless regions
+	return getMapKey(
+		region.flags[moduleName]?.[regionFlags.terrainTypeId],
+		region._source.elevation.top,
+		region._source.elevation.bottom
+	);
+}
+
+/** @param {TerrainShape} shape */
+function getMapKeyForShape(shape) {
+	// Regions have a null top/bottom if they are infinite, so mimic that behavior
+	const usesHeight = shape.terrainType?.usesHeight;
+	return getMapKey(
+		shape.terrainTypeId,
+		usesHeight ? shape.top : null,
+		usesHeight ? shape.bottom : null
+	);
+}
+
 function getMapKey(terrainTypeId, top, bottom) {
 	return `${terrainTypeId}|${top}|${bottom}`;
 }

src/geometry/terrain-shape.mjs
@@ -102,19 +102,19 @@ export class TerrainShape {
 	}
 
 	get height() {
-		return this.terrainType?.usesHeight ? this.#height : null;
+		return this.#height;
 	}
 
 	get elevation() {
-		return this.terrainType?.usesHeight ? this.#elevation : null;
+		return this.#elevation;
 	}
 
 	get top() {
-		return this.terrainType?.usesHeight ? this.#elevation + this.#height : null;
+		return this.#elevation + this.#height;
 	}
 
 	get bottom() {
-		return this.terrainType?.usesHeight ? this.#elevation : null;
+		return this.#elevation;
 	}
 
 	get visible() {

commit eb9f2d0dd78df870171bfd289747779782d43ad0

Fix scene regions not being created for zones
Regression from 120b7253c5d1f4bfd4247ff38110e36854d66ab4

src/automation/scene-regions.mjs
@@ -82,7 +82,10 @@ async function createOrUpdateRegions(terrainShapes, cleanup = false) {
 	// Feels better to have one region per group of shapes, rather than one region per shape? Not sure if it will affect
 	// performance by any significant amount though?
 	const changedRegions = distinctBy(terrainShapes, s => s.terrainTypeId, s => s.top, s => s.bottom)
-		.map(({ terrainTypeId, top, bottom }) => ({ terrainTypeId, top, bottom }));
+		.map(({ terrainTypeId, top, bottom }) => {
+			const usesHeight = terrainTypeMap$.value.get(terrainTypeId)?.usesHeight;
+			return { terrainTypeId, top: usesHeight ? top : null, bottom: usesHeight ? bottom : null };
+		});
 
 	/** @type {Map<string, RegionDocument>} */
 	const existingRegionsLookup = new Map(getThtSceneRegions().map(r => [getMapKeyForRegion(r), r]));